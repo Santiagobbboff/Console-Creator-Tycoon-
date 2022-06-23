f44961a1019aa95d83e22e8906c1d85d9f0fd192
Santiagobbboff <sheredergamer@gmail.com>
2022-06-23 02:43:47 +0100

    Novo script adicionado
    nao funciona

---
+++ Console Creator Tycoon/Assets/entrarnasdefinicoes.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class definicoes : MonoBehaviour
+{
+
+    public void entrarnasdefinicoes()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+}
