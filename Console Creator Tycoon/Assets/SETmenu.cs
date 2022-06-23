1bd65638bffcd7e1a2a3579b918beee815089975
RomanMANO <108025197+RomanMANO@users.noreply.github.com>
2022-06-23 18:01:38 +0100

    script teste

---
+++ Console Creator Tycoon/Assets/SETmenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SETmenu : MonoBehaviour
+{
+    public void SetFullscreen(bool isFullscreen)
+    {
+        isFullscreen = isFullscreen;
+    }
+}
